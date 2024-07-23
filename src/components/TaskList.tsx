433002c1482cdc743cef13e50ec232a5b385dda0
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-07-24 00:33:40 +0330

    [REFACTOR] clean up the template

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -12,23 +12,33 @@ const TaskList = ({ tasks, removeTask, toggleTask }: TaskListProps) => {
 
     return (
         <>
-            {tasks.length > 0 ? (
+            {tasks.length > 0 && (
                 <>
                     <h1 className="my-8">Tasks to do - {pendingTasks.length}</h1>
                     <div className="flex flex-col">
-                        {pendingTasks.map((task) => (
-                            <Task key={task.id} task={task} removeTask={removeTask} toggleTask={toggleTask} />
+                        {pendingTasks.map(task => (
+                            <Task
+                                key={task.id}
+                                task={task}
+                                removeTask={removeTask}
+                                toggleTask={toggleTask}
+                            />
                         ))}
                     </div>
 
                     <h2 className="my-8 mt-20">Done - {completedTasks.length}</h2>
                     <div className="flex flex-col">
-                        {completedTasks.map((task) => (
-                            <Task key={task.id} task={task} removeTask={removeTask} toggleTask={toggleTask} />
+                        {completedTasks.map(task => (
+                            <Task
+                                key={task.id}
+                                task={task}
+                                removeTask={removeTask}
+                                toggleTask={toggleTask}
+                            />
                         ))}
                     </div>
                 </>
-            ) : <></>}
+            )}
         </>
     );
 }
