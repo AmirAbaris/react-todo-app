c216bc3655b47422921adc44d1f187367c77d83a
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-07-24 00:45:21 +0330

    [REFACTOR] added helper method to avoid duplicated code

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,32 +10,30 @@ const TaskList = ({ tasks, removeTask, toggleTask }: TaskListProps) => {
     const pendingTasks = tasks.filter(task => !task.completed);
     const completedTasks = tasks.filter(task => task.completed);
 
+    // helper function to render tasks
+    const renderTasks = (taskList: TaskType[]) => (
+        taskList.map(task => (
+            <Task
+                key={task.id}
+                task={task}
+                removeTask={removeTask}
+                toggleTask={toggleTask}
+            />
+        ))
+    );
+
     return (
         <>
             {tasks.length > 0 && (
                 <>
                     <h1 className="my-8">Tasks to do - {pendingTasks.length}</h1>
                     <div className="flex flex-col">
-                        {pendingTasks.map(task => (
-                            <Task
-                                key={task.id}
-                                task={task}
-                                removeTask={removeTask}
-                                toggleTask={toggleTask}
-                            />
-                        ))}
+                        {renderTasks(pendingTasks)}
                     </div>
 
                     <h2 className="my-8 mt-20">Done - {completedTasks.length}</h2>
                     <div className="flex flex-col">
-                        {completedTasks.map(task => (
-                            <Task
-                                key={task.id}
-                                task={task}
-                                removeTask={removeTask}
-                                toggleTask={toggleTask}
-                            />
-                        ))}
+                        {renderTasks(completedTasks)}
                     </div>
                 </>
             )}
