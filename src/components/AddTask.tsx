0dfefc83cda4dc73c9c33f29645c3e0dc33a323a
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-07-24 18:20:27 +0330

    [UPDATE] added useReducer for input value

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,29 +1,45 @@
 import { Button, Input } from "@nextui-org/react";
-import { useState } from "react";
+import { useReducer } from "react";
 
 interface AddTaskProps {
     addTask: (task: string) => void;
 }
 
+type State = {
+    task: string;
+};
+
+type Action =
+    | { type: 'SET_TASK', payload: string }
+    | { type: 'RESET_TASK' };
+
+const reducer = (state: State, action: Action): State => {
+    switch (action.type) {
+        case 'SET_TASK':
+            return { ...state, task: action.payload };
+        case 'RESET_TASK':
+            return { ...state, task: '' };
+    }
+};
+
 const AddTask = ({ addTask }: AddTaskProps) => {
-    const [task, setTask] = useState<string>('');
+    const [state, dispatch] = useReducer(reducer, { task: '' });
 
     const handleSubmit = (e: React.FormEvent): void => {
         e.preventDefault();
-        if (task.trim()) {
-            addTask(task);
-            setTask('');
+        if (state.task.trim()) {
+            addTask(state.task);
+            dispatch({ type: 'RESET_TASK' });
         }
-
     };
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setTask(e.target.value);
+        dispatch({ type: 'SET_TASK', payload: e.target.value });
     };
 
     return (
         <form className="flex items-center gap-x-6" onSubmit={handleSubmit}>
-            <Input type="text" label="Add a new Task" value={task} onChange={handleChange} />
+            <Input type="text" label="Add a new Task" value={state.task} onChange={handleChange} />
             <Button color="secondary" type="submit">
                 Add Task
             </Button>
